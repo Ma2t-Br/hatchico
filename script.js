1b8f1a20ca8fdc3f43cc3c630fcba25785bc1208
Ma2t-Br <matthieu.brandao@efrei.net>
2024-09-05 14:41:46 +0200

    Version 1.3.0 cleaning

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
 import { getDatabase, ref, push, query, limitToLast, onValue } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 
-/* *** Firebase ******************************************************************************************** */
-
-// Configuration Firebase
+/* *** Firebase Configuration & Initialization ************************************************************ */
 const firebaseConfig = {
     apiKey: "AIzaSyDTUMai-OpmnWaGXYrpIXCsa7ZN4v-ovzM",
     authDomain: "hatchico-b166b.firebaseapp.com",
@@ -16,114 +14,25 @@ const firebaseConfig = {
     databaseURL: "https://hatchico-b166b-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
-// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
 
-
-/* *** Data gets ******************************************************************************************** */
-
-// Date
+/* *** DOM Elements & Constants ********************************************************************** */
 const dateElement = document.getElementById('date');
 const today = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-// Formulaire
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const optionsWithDay = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const form = document.getElementById('mealForm');
 const modal = document.getElementById('nameModal');
 const closeBtn = document.getElementsByClassName('close')[0];
 const modalSubmit = document.getElementById('modalSubmit');
 const nameSelect = document.getElementById('nameSelect');
-
-// Liste des noms à suivre
 const namesToTrack = ["Aurélie", "Chloé", "Claire", "Marie-lou", "Matthieu", "Nicolas", "Philippe", "Thibault", "Vincent"];
 
-// Récupérer la dernière entrée de la base de données
-const getLastEntry = () => {
-    const lastEntryQuery = query(ref(database, 'meals'), limitToLast(1));
-    onValue(lastEntryQuery, (snapshot) => {
-        let lastEntry = null;
-        snapshot.forEach((childSnapshot) => {
-            lastEntry = childSnapshot.val();
-        });
-        displayLastEntry(lastEntry);
-        checkLastEntryAndAdjustOptions(lastEntry)
-    });
-};
-
-/* *** Outils ******************************************************************************************** */
-
-// Fonction pour compter les points
-const countPoints = () => {
-    const points = {};
-
-    // Initialiser les points à 0 pour chaque nom
-    namesToTrack.forEach(name => {
-        points[name] = 0;
-    });
-
-    // Récupérer toutes les entrées de la base de données
-    const entriesQuery = ref(database, 'meals');
-    onValue(entriesQuery, (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-            const entry = childSnapshot.val();
-            if (namesToTrack.includes(entry.name)) {
-                points[entry.name]++;
-            }
-        });
-
-        // Afficher le tableau des points
-        displayPoints(points);
-    });
-};
-
-// Pop-up de nom et soumettre les données à Firebase
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche l'envoi du formulaire
-    modal.style.display = 'block';
-});
-
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
-};
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-};
-
-modalSubmit.addEventListener('click', function() {
-    const personName = nameSelect.value;
-    const mealInput = form.querySelector('input[name="meal"]:checked');
-    if (mealInput) {
-        const mealValue = mealInput.value;
-        const dateTimeString = today.toLocaleString('fr-FR', {
-            year: 'numeric', month: 'long', day: 'numeric',
-            hour: 'numeric', minute: 'numeric', second: 'numeric'
-        });
-
-        // Enregistrer les données dans Firebase
-        push(ref(database, 'meals'), {
-            name: personName,
-            meal: mealValue,
-            dateTime: dateTimeString
-        }).then(() => {
-            alert(`Merci, ${personName}!`);
-            modal.style.display = 'none';
-            // Mettre à jour l'affichage avec la nouvelle entrée
-            getLastEntry();
-        }).catch((error) => {
-            console.error('Erreur:', error);
-            alert('Une erreur est survenue lors de l\'enregistrement des données.');
-        });
-    } else {
-        alert("Veuillez sélectionner un repas.");
-    }
-});
-
-// Fonction pour convertir une date en chaîne de caractères en un objet Date
+/* *** Utility Functions ******************************************************************************* */
+const formatDate = (date) => date.toLocaleDateString('fr-FR', options);
+const formatDateWithDay = (date) => date.toLocaleDateString('fr-FR', optionsWithDay);
 const parseDateString = (dateString) => {
     const [day, month, year] = dateString.split(' ');
     const months = {
@@ -133,23 +42,65 @@ const parseDateString = (dateString) => {
     return new Date(`${year}-${months[month] + 1}-${day}`);
 };
 
-/* *** Section Constructors ******************************************************************************************** */
+/* *** Firebase Data Handling ************************************************************************* */
+const fetchData = (callback) => {
+    const dataQuery = ref(database, 'meals');
+    onValue(dataQuery, (snapshot) => {
+        const data = [];
+        snapshot.forEach(childSnapshot => {data.push(childSnapshot.val())});
+        callback(data);
+    });
 
-// Afficher la date actuelle
-dateElement.textContent = today.toLocaleDateString('fr-FR', options);
+};
 
-// Vérifier la dernière entrée et ajuster les options
+const getLastEntry = () => {
+    const lastEntryQuery = query(ref(database, 'meals'), limitToLast(1));
+    onValue(lastEntryQuery, (snapshot) => {
+        let lastEntry = null;
+        snapshot.forEach(childSnapshot => lastEntry = childSnapshot.val());
+        displayLastEntry(lastEntry);
+        checkLastEntryAndAdjustOptions(lastEntry);
+    });
+};
+
+const countPoints = () => {
+    fetchData(entries => {
+        const points = namesToTrack.reduce((acc, name) => ({ ...acc, [name]: 0 }), {});
+        entries.forEach(entry => {
+            if (namesToTrack.includes(entry.name)) {
+                points[entry.name]++;
+            }
+        });
+        displayPoints(points);
+    });
+};
+
+/* *** DOM Manipulation Functions ******************************************************************** */
+const displayLastEntry = (entry) => {
+    const lastEntryElement = document.getElementById('lastEntry');
+    if (entry) {
+        const { name, meal, dateTime } = entry;
+        lastEntryElement.innerHTML = `Dernier donateur : ${name} le ${meal} <br>(${dateTime})`;
+    } else {
+        lastEntryElement.textContent = "Pas encore de données disponibles.";
+    }
+};
+
 const checkLastEntryAndAdjustOptions = (entry) => {
     const matinOption = document.getElementById('matinLabel');
     const soirOption = document.getElementById('soirLabel');
     const validerButton = document.getElementById('valider');
-    const title = document.getElementById('title')
+    const title = document.getElementById('title');
+    const todayDate = formatDate(today);
 
     if (entry) {
         const { meal, dateTime } = entry;
         const lastEntryDate = dateTime.split(' à ')[0];
 
-        if (lastEntryDate === today.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' })) {
+        console.log('todayDate:', todayDate);
+        console.log('lastEntryDate:', lastEntryDate);
+        console.log('todayDate === lastEntryDate:', todayDate === lastEntryDate);
+        if (lastEntryDate === todayDate) {
             if (meal === 'matin') {
                 matinOption.style.display = 'none';
                 soirOption.style.display = 'block';
@@ -169,18 +120,6 @@ const checkLastEntryAndAdjustOptions = (entry) => {
     }
 };
 
-// Afficher la dernière entrée
-const displayLastEntry = (entry) => {
-    const lastEntryElement = document.getElementById('lastEntry');
-    if (entry) {
-        const { name, meal, dateTime } = entry;
-        lastEntryElement.innerHTML = `Dernier donateur : ${name} le ${meal} <br>(${dateTime})`;
-    } else {
-        lastEntryElement.textContent = "Pas encore de données disponibles.";
-    }
-};
-
-// Afficher le tableau des points
 const displayPoints = (points) => {
     const pointsCounterElement = document.getElementById('pointsCounter');
     pointsCounterElement.innerHTML = '<h3>Compteur de Points</h3>';
@@ -198,17 +137,7 @@ const displayPoints = (points) => {
         const nameCell = document.createElement('td');
         nameCell.style.padding = '8px';
         nameCell.style.textAlign = 'left';
-
-        // Ajouter les icônes en fonction de la position
-        if (index === 0) {
-            nameCell.innerHTML = `${name} <img src="Img/os or.png" alt="🥇" style="width:18px; height:18px; vertical-align:middle;">`; // Os d'or
-        } else if (index === 1) {
-            nameCell.innerHTML = `${name} <img src="Img/os argent.png" alt="🥈" style="width:18px; height:18px; vertical-align:middle;">`; // Os d'argent
-        } else if (index === 2) {
-            nameCell.innerHTML = `${name} <img src="Img/os bronze.png" alt="🥉" style="width:18px; height:18px; vertical-align:middle;">`; // Os de bronze
-        } else {
-            nameCell.textContent = name;
-        }
+        nameCell.innerHTML = `${name} ${index < 3 ? `<img src="Img/os ${['or', 'argent', 'bronze'][index]}.png" alt="🥇🥈🥉".charAt(index)}" style="width:18px; height:18px; vertical-align:middle;">` : ''}`;
 
         const scoreCell = document.createElement('td');
         scoreCell.textContent = score;
@@ -223,105 +152,99 @@ const displayPoints = (points) => {
     pointsCounterElement.appendChild(pointsTable);
 };
 
-/* *** Calls ******************************************************************************************** */
-
-// Appel initial pour récupérer et afficher la dernière entrée
-getLastEntry();
-
-// Appel initial pour récupérer et afficher le compteur de points
-countPoints();
-
-
-
-////
-
 const displayHistory = (entries) => {
     const historyContainer = document.getElementById('historyContainer');
-    historyContainer.innerHTML = ''; // Vider le conteneur pour réinitialiser l'affichage
+    historyContainer.innerHTML = ''; // Clear container
 
-    const groupedByDate = {};
+    const groupedByDate = entries.reduce((acc, entry) => {
+        const entryDate = entry.dateTime.split(' à ')[0];
+        if (!acc[entryDate]) {
+            acc[entryDate] = { matin: false, soir: false, entries: [] };
+        }
+        if (entry.meal === 'matin') acc[entryDate].matin = true;
+        if (entry.meal === 'soir') acc[entryDate].soir = true;
+        acc[entryDate].entries.push(entry);
+        return acc;
+    }, {});
 
-    // Grouper les entrées par jour
-    entries.forEach((entry) => {
-        const entryDate = entry.dateTime.split(' à ')[0]; // Extraire la date sans l'heure
+    const sortedDates = Object.keys(groupedByDate)
+        .map(date => ({ date, value: parseDateString(date) }))
+        .sort((a, b) => b.value - a.value)
+        .map(item => item.date);
 
-        if (!groupedByDate[entryDate]) {
-            groupedByDate[entryDate] = { matin: false, soir: false, entries: [] };
+    sortedDates.forEach(date => {
+        const dayData = groupedByDate[date];
+        const dayDiv = document.createElement('div');
+        dayDiv.classList.add('day-history');
+
+        if (date !== formatDate(today)) {
+            const badge = document.createElement('span');
+            badge.classList.add('badge');
+            badge.classList.add(dayData.matin && dayData.soir ? 'badge-green' : 'badge-red');
+            dayDiv.appendChild(badge);
         }
 
-        // Vérifier si l'entrée est pour le matin ou le soir
-        if (entry.meal === 'matin') {
-            groupedByDate[entryDate].matin = true;
-        } else if (entry.meal === 'soir') {
-            groupedByDate[entryDate].soir = true;
-        }
+        const dayHeader = document.createElement('h3');
+        dayHeader.textContent = date;
+        dayDiv.appendChild(dayHeader);
 
-        groupedByDate[entryDate].entries.push(entry); // Ajouter l'entrée à la liste du jour
+        dayData.entries.forEach(entry => {
+            const entryDiv = document.createElement('div');
+            entryDiv.textContent = `${entry.meal} : ${entry.name}`;
+            dayDiv.appendChild(entryDiv);
+        });
+
+        historyContainer.appendChild(dayDiv);
     });
 
-    // Trier les dates du plus récent au plus ancien
-    const dates = Object.keys(groupedByDate)
-        .map(date => ({ date, value: parseDateString(date) })) // Convertir en objets Date
-        .sort((a, b) => b.value - a.value) // Trier les objets Date
-        .map(item => item.date); // Extraire les dates triées
+    if (sortedDates.length === 0) historyContainer.style.display = 'none';
+};
 
-    if (dates.length > 0) {
-        dates.forEach((date) => {
-            const dayData = groupedByDate[date];
-            const dayDiv = document.createElement('div');
-            dayDiv.classList.add('day-history');
+/* *** Event Listeners ******************************************************************************** */
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    modal.style.display = 'block';
+});
 
-            // Créer la pastille pour indiquer l'état (matin et soir ou manquant)
-            if (date !== today.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' })) {
-                // Créer la pastille pour indiquer l'état (matin et soir ou manquant)
-                const badge = document.createElement('span');
-                badge.classList.add('badge');
-                if (dayData.matin && dayData.soir) {
-                    badge.classList.add('badge-green'); // Matin et soir présents
-                } else {
-                    badge.classList.add('badge-red'); // Manque soit matin soit soir
-                }
+closeBtn.onclick = () => modal.style.display = 'none';
 
-                // Ajouter la pastille au jour
-                dayDiv.appendChild(badge);
-            }
-
-            // Créer l'en-tête avec la date
-            const dayHeader = document.createElement('h3');
-            dayHeader.textContent = date;
-            dayDiv.appendChild(dayHeader);
-
-            // Ajouter les entrées (matin et soir) sous chaque jour
-            dayData.entries.forEach((entry) => {
-                const entryDiv = document.createElement('div');
-                entryDiv.textContent = `${entry.meal} : ${entry.name}`;
-                dayDiv.appendChild(entryDiv);
-            });
-
-            // Ajouter le jour en haut de la liste
-            historyContainer.appendChild(dayDiv);
-        });
-    } else {
-        historyContainer.style.display = 'none';
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
     }
 };
 
-
-
-// Fonction pour récupérer l'historique de la base de données
-const getHistory = () => {
-    const historyQuery = ref(database, 'meals');
-    onValue(historyQuery, (snapshot) => {
-        const entries = [];
-        snapshot.forEach((childSnapshot) => {
-            const entry = childSnapshot.val();
-            entries.push(entry);
+modalSubmit.addEventListener('click', () => {
+    const personName = nameSelect.value;
+    const mealInput = form.querySelector('input[name="meal"]:checked');
+    if (mealInput) {
+        const mealValue = mealInput.value;
+        const dateTimeString = today.toLocaleString('fr-FR', {
+            year: 'numeric', month: 'long', day: 'numeric',
+            hour: 'numeric', minute: 'numeric', second: 'numeric'
         });
 
-        // Afficher l'historique une fois les données récupérées
-        displayHistory(entries);
-    });
-};
+        push(ref(database, 'meals'), {
+            name: personName,
+            meal: mealValue,
+            dateTime: dateTimeString
+        }).then(() => {
+            alert(`Merci, ${personName}!`);
+            modal.style.display = 'none';
+            getLastEntry();
+            countPoints();
+            fetchData(displayHistory); // Update history after adding new entry
+        }).catch((error) => {
+            console.error('Erreur:', error);
+            alert('Une erreur est survenue lors de l\'enregistrement des données.');
+        });
+    } else {
+        alert("Veuillez sélectionner un repas.");
+    }
+});
 
-// Appel initial pour récupérer et afficher l'historique
-getHistory();
+/* *** Initial Calls ********************************************************************************** */
+dateElement.textContent = formatDateWithDay(today);
+getLastEntry();
+countPoints();
+fetchData(displayHistory);
